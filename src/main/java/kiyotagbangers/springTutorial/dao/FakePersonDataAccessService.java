abd84e07d47c37bea664e46e7b47232b4a249432
kiyota takeshi <kiyota@tagbangers.co.jp>
2020-08-15 10:26:17 +0900

    add PUT method
    - PUT
    
    ```shell
    $ curl --location --request PUT 'http://localhost:8080/api/v1/person/9573bb6d-9d75-45a4-9ba7-dae9c5605163' \
    --header 'Content-Type: application/json' \
    --data-raw '{
        "name": "Updated"
    }'
    
    $ curl --location --request GET 'http://localhost:8080/api/v1/person'
    [
        {
            "id": "9573bb6d-9d75-45a4-9ba7-dae9c5605163",
            "name": "Updated"
        }
    ]
    ```

--- src/main/java/kiyotagbangers/springTutorial/dao/FakePersonDataAccessService.java
+++ src/main/java/kiyotagbangers/springTutorial/dao/FakePersonDataAccessService.java
@@ -36,11 +36,25 @@ public class FakePersonDataAccessService implements PersonDao {
 
     @Override
     public int deletePersonById(UUID id) {
-        return 0;
+        Optional<Person> personMaybe = selectPersonById(id);
+        if (personMaybe.isEmpty()){
+            return 0;
+        }
+        DB.remove(personMaybe.get());
+        return 1;
     }
 
     @Override
-    public int updatePersonById(UUID id, Person person) {
-        return 0;
+    public int updatePersonById(UUID id, Person update) {
+        return selectPersonById(id)
+                .map(p -> {
+                    int indexOfPersonToUpdate = DB.indexOf(p);
+                    if (indexOfPersonToUpdate >= 0){
+                        DB.set(indexOfPersonToUpdate, new Person(id, update.getName()));
+                        return 1;
+                    }
+                    return 0;
+                })
+                .orElse(0);
     }
 }
