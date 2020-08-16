c40771479f8ef169eac087ce9e286a2cdff9f79a
Kiyota iMac <kiyotatakeshi0609@gmail.com>
2020-08-16 20:54:20 +0900

    add validation(NotNull and Valid)

--- src/main/java/kiyotagbangers/springTutorial/api/PersonController.java
+++ src/main/java/kiyotagbangers/springTutorial/api/PersonController.java
@@ -3,8 +3,10 @@ package kiyotagbangers.springTutorial.api;
 import kiyotagbangers.springTutorial.model.Person;
 import kiyotagbangers.springTutorial.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.UUID;
 
@@ -23,7 +25,7 @@ public class PersonController {
     }
 
     @PostMapping
-    public void addPerson(@RequestBody Person person){
+    public void addPerson(@Valid @NonNull  @RequestBody Person person){
         personService.addPerson(person);
     }
 
@@ -44,7 +46,7 @@ public class PersonController {
     }
 
     @PutMapping(path = "{id}")
-    public void updatePerson(@PathVariable("id") UUID id, @RequestBody Person personToUpdate){
+    public void updatePerson(@PathVariable("id") UUID id,@Valid @NonNull @RequestBody Person personToUpdate){
         personService.updatePerson(id, personToUpdate);
     }
 }
